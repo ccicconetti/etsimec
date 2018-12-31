5a4e367ba699a549218f7370c69cc473e6d72e52
Claudio Cicconetti <ccicconetti@mbigroup.it>
2018-12-31 11:09:52 +0100

    Added license boilerplate.

--- EtsiMec/utils.cpp
+++ EtsiMec/utils.cpp
@@ -1,3 +1,32 @@
+/*
+ ___ ___ __     __ ____________
+|   |   |  |   |__|__|__   ___/  Ubiquitout Internet @ IIT-CNR
+|   |   |  |  /__/  /  /  /      C++ ETSI MEC library
+|   |   |  |/__/  /   /  /       https://bitbucket.org/ccicconetti/etsimec/
+|_______|__|__/__/   /__/
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+Copyright (c) 2018 Claudio Cicconetti <https://about.me/ccicconetti>
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "utils.h"
 
 #include "EtsiMec/ueapplcmproxy.h"
@@ -5,7 +34,7 @@
 namespace uiiit {
 namespace etsimec {
 
-std::string removeTrailingSlashes(const std::string& aValue) {
+std::string removeTrailingSlashes(const std::string &aValue) {
   auto ret(aValue);
   while (not ret.empty() and ret.back() == '/') {
     ret.pop_back();
@@ -13,7 +42,7 @@ std::string removeTrailingSlashes(const std::string& aValue) {
   return ret;
 }
 
-std::string mx2root(const std::string& aApiRoot) {
+std::string mx2root(const std::string &aApiRoot) {
   return removeTrailingSlashes(aApiRoot) + "/" +
          UeAppLcmProxy::staticApiName() + "/" +
          UeAppLcmProxy::staticApiVersion();
