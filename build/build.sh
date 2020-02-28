d86e11793a82e55037643ab5e93eb2221ceb20fd
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2020-02-28 08:44:38 +0100

    Update test-related and cmake and scripts

--- build/build.sh
+++ build/build.sh
@@ -6,8 +6,16 @@ if [ $# -ne 1 ] ; then
 fi
 
 if [ ! -z "$( ls -A | grep -v .gitignore)" ]; then
-  echo "Directory not empty, leaving now"
-  exit 1
+  read -p "Directory not empty, remove everything? (say \"yes\" if sure) "
+  if [ "$REPLY" == "yes" ] ; then
+    read -n 1 -s -p "This will remove EVERYTHING in the current directory, Ctrl+C to leave NOW"
+    rm -rf *
+    echo
+    echo "Done, the directory should now be empty"
+  else
+    echo "OK, leaving now"
+    exit 1
+  fi
 fi
 
 CURRENTDIR=${PWD##*/}
